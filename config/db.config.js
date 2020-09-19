b34ce40210764ebf3a7fc13efc9dc273d8fd652d
Benk-Office <mail.buat.forum@gmail.com>
2020-09-19 15:16:17 +0700

    ubah config

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "119.47.92.38",
-    USER: "developer",
-    PASSWORD: "d3v3l0p3r",
-    DB: "apotik",
+    HOST: "-",
+    USER: "-",
+    PASSWORD: "-",
+    DB: "-",
 }
